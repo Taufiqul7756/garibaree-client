ca5f9bd4bbdc3ddc31413286aee420ddf2fe87f8
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-07 17:43:11 +0600

    routing

--- client/app/page.js
+++ client/app/page.js
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+  return <main className="p-24 bg-red-600">Hello</main>;
 }
