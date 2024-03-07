314737418246f35b1e3d7db9d077b0c68bd60cc8
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-07 17:51:19 +0600

    bottom section of NavBar - demo added

--- client/app/components/Navbar.jsx
+++ client/app/components/Navbar.jsx
@@ -4,17 +4,47 @@ import gariBareeLogo from "../images/garibareeLogo.png";
 import logo from "../images/logo.png";
 import { GoBell } from "react-icons/go";
 import { MdFavoriteBorder } from "react-icons/md";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <>
-      <div className="py-4 px-5">
-        <div className="flex justify-between items-center">
-          <Image src={gariBareeLogo} />
+      <div className="py-5 px-5">
+        {/* upper NavBar */}
+        <div className="flex justify-between items-center mb-3">
+          <Link href="/">
+            <Image src={gariBareeLogo} />
+          </Link>
           <div className="flex justify-between items-center gap-5">
-            <GoBell />
-            <MdFavoriteBorder />
-            <Image src={logo} height={32} width={32} />
+            <Link href="/" className="">
+              <GoBell />
+            </Link>
+            <Link href="/">
+              <MdFavoriteBorder />
+            </Link>
+
+            <Link href="/">
+              <Image src={logo} height={32} width={32} />
+            </Link>
+          </div>
+        </div>
+        <hr />
+        {/* bottom Navbar */}
+        <div className="flex justify-between items-center">
+          <Link href="/">
+            <Image src={gariBareeLogo} />
+          </Link>
+          <div className="flex justify-between items-center gap-5">
+            <Link href="/" className="">
+              <GoBell />
+            </Link>
+            <Link href="/">
+              <MdFavoriteBorder />
+            </Link>
+
+            <Link href="/">
+              <Image src={logo} height={32} width={32} />
+            </Link>
           </div>
         </div>
       </div>
