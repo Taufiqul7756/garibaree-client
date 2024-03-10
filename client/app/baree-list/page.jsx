93faac320a6b5712072ff26e846f359211b3e23c
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-11 04:00:20 +0600

    give margin top

---
+++ client/app/baree-list/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BareeListPage from "./BareeListPage";
+
+const page = () => {
+  return (
+    <div>
+      <BareeListPage />
+    </div>
+  );
+};
+
+export default page;
