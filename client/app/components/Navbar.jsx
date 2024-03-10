9316a84be8daad0c62d67ed141698d8fa8f7dfb9
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-11 03:59:59 +0600

    fixed

--- client/app/components/Navbar.jsx
+++ client/app/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     {
       label: "Property",
       dropdown: [
-        "All Listings",
+        "Baree List",
         "For Sale",
         "For Rent",
         "Bookerge",
@@ -25,7 +25,7 @@ const Navbar = () => {
     },
     {
       label: "Motor",
-      dropdown: ["All List"],
+      dropdown: ["Gari List"],
     },
     {
       label: "Dealers",
@@ -54,53 +54,68 @@ const Navbar = () => {
   };
   return (
     <>
-      <div className="py-5 px-16">
-        {/* upper NavBar */}
-        <div className="flex justify-between items-center mb-3">
-          <Link href="/">
-            <Image src={gariBareeLogo} />
-          </Link>
-          <div className="flex justify-between items-center gap-5">
-            <Link href="/" className="">
-              <GoBell />
-            </Link>
+      <div className="fixed top-0 left-0 right-0 bg-white shadow-md z-10">
+        <div className="py-5 px-16">
+          {/* upper NavBar */}
+          <div className="flex justify-between items-center mb-3">
             <Link href="/">
-              <MdFavoriteBorder />
+              <Image src={gariBareeLogo} />
             </Link>
-
-            <Link href="/">
-              <Image src={logo} height={32} width={32} />
-            </Link>
-          </div>
-        </div>
-        <hr />
-        {/* bottom Navbar */}
-        <div className="py-2">
-          <div className="flex justify-between items-center mt-5">
-            {NavItems.map((item) => (
-              <div
-                key={item.label}
-                className="relative group flex items-center"
-                onMouseEnter={() => handleMouseEnter(item.label)}
-                onMouseLeave={handleMouseLeave}
-              >
-                <span className="cursor-pointer">{item.label}</span>
-                {item.dropdown.length > 0 && <FaAngleDown className="ml-1" />}
-                {item.dropdown.length > 0 && activeDropdown === item.label && (
-                  <div className="absolute left-0 top-full mt-1 bg-slate-200 border border-gray-200 py-5 px-12 rounded shadow-lg">
-                    {item.dropdown.map((dropdownItem) => (
-                      <div key={dropdownItem}>{dropdownItem}</div>
-                    ))}
-                  </div>
-                )}
-              </div>
-            ))}
-            <div className="bg-blue-600 rounded-full py-2 px-5">
-              {" "}
-              <Link href="/">
-                {" "}
-                <button>Post An ad</button>{" "}
+            <div className="flex justify-between items-center gap-5">
+              <Link href="/" className="">
+                <GoBell />
               </Link>
+              <Link href="/">
+                <MdFavoriteBorder />
+              </Link>
+
+              <Link href="/">
+                <Image src={logo} height={32} width={32} />
+              </Link>
+            </div>
+          </div>
+          <hr />
+          {/* bottom Navbar */}
+          <div className="py-2">
+            <div className="flex justify-between items-center mt-5">
+              <div className="flex gap-2 md:gap-5 lg:gap-24 ">
+                {NavItems.map((item) => (
+                  <div
+                    key={item.label}
+                    className="relative group flex items-center "
+                    onMouseEnter={() => handleMouseEnter(item.label)}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    <span className="cursor-pointer">{item.label}</span>
+                    {item.dropdown.length > 0 && (
+                      <FaAngleDown className="ml-1" />
+                    )}
+                    {item.dropdown.length > 0 &&
+                      activeDropdown === item.label && (
+                        <div className="absolute left-0 top-full mt-1 bg-slate-200 border border-gray-200  rounded shadow-lg ">
+                          {item.dropdown.map((dropdownItem) => (
+                            <Link
+                              className="block py-2 px-16 hover:bg-gray-300"
+                              key={dropdownItem}
+                              href={`/${dropdownItem
+                                .toLowerCase()
+                                .replace(" ", "-")}`}
+                            >
+                              {dropdownItem}
+                            </Link>
+                          ))}
+                        </div>
+                      )}
+                  </div>
+                ))}
+              </div>
+              <div className="bg-blue-600 rounded-full py-2 px-5">
+                {" "}
+                <Link href="/">
+                  {" "}
+                  <button>Post An ad</button>{" "}
+                </Link>
+              </div>
             </div>
           </div>
         </div>
